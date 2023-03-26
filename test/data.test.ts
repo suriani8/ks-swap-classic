25ea32b88ee15e191eb9ca70c6c20821f0951b52
Coffee Crypto Indo <coffeecryptoindo@gmail.com>
2023-03-27 05:37:17 +0800

    new

---
+++ test/data.test.ts
@@ -0,0 +1,15 @@
+import { Fetcher } from '../src'
+import { ChainId } from '@kadafial/ks-sdk-core-kadafi'
+
+// TODO: replace the provider in these tests
+describe('data', () => {
+  it('Token', async () => {
+    const token = await Fetcher.fetchTokenData(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F') // DAI
+    expect(token.decimals).toEqual(18)
+  })
+
+  it('Token:CACHE', async () => {
+    const token = await Fetcher.fetchTokenData(ChainId.MAINNET, '0xE0B7927c4aF23765Cb51314A0E0521A9645F0E2A') // DGD
+    expect(token.decimals).toEqual(9)
+  })
+})
